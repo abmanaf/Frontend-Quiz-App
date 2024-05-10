0eb1cfed643e7a4ac904a3a68ec895f2f35aaea7
Alibaba <abdulmanafaliu414@gmail.com>
2024-05-10 12:17:49 +0000

    minor changes

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -13,8 +13,8 @@ const StyledConMain = styled.div<StyledConMainProps>`
   background: ${(props) =>
     `var(--cl-bg-main) ${
       props.$isDarkMode
-        ? "url('pattern-background-mobile-dark.svg')"
-        : "url('pattern-background-mobile-light.svg')"
+        ? "url('/assets/images/pattern-background-mobile-dark.svg')"
+        : "url('/assets/images/pattern-background-mobile-light.svg')"
     } no-repeat center`};
 
   ${flex}
@@ -24,16 +24,16 @@ const StyledConMain = styled.div<StyledConMainProps>`
     background: ${(props) =>
       `var(--cl-bg-main) ${
         props.$isDarkMode
-          ? "url('pattern-background-tablet-dark.svg')"
-          : "url('pattern-background-tablet-light.svg')"
+          ? "url('/assets/images/pattern-background-tablet-dark.svg')"
+          : "url('/assets/images/pattern-background-tablet-light.svg')"
       } no-repeat left top`};
   }
   @media (min-width: 1024px) {
     background: ${(props) =>
       `var(--cl-bg-main) ${
         props.$isDarkMode
-          ? "url('pattern-background-desktop-dark.svg')"
-          : "url('pattern-background-desktop-light.svg')"
+          ? "url('/assets/images/pattern-background-desktop-dark.svg')"
+          : "url('/assets/images/pattern-background-desktop-light.svg')"
       } no-repeat left top`};
     background-size: cover;
   }
